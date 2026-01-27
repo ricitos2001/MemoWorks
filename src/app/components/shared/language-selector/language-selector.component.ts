be6e7dbc4eb16835e9c70568272fe1be5ba40cc3
ricitos2001 <cesar2001ricitos@gmail.com>
2026-01-27 20:41:43 +0100

    pequeña correcion de accesibilidad

--- src/app/components/shared/language-selector/language-selector.component.ts
+++ src/app/components/shared/language-selector/language-selector.component.ts
@@ -2,25 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import {LanguageService} from '../../../services/language.service';
 import {NgForOf} from '@angular/common';
 import {FormInputComponent} from '../form-input/form-input.component';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-selector',
   imports: [
     NgForOf,
-    FormInputComponent
+    TranslateModule,
   ],
   templateUrl: './language-selector.component.html',
   styleUrl: '../../../../styles/styles.css',
 })
 export class LanguageSelectorComponent implements OnInit {
 
-  constructor(private languageService: LanguageService) {
+  constructor(private languageService: LanguageService, private translate: TranslateService) {
   }
 
   languages = [
     { code: 'es', label: 'Español'},
     { code: 'en', label: 'English'}
   ];
+
+  languageLabel = '';
 
   currentLang!: string;
 
@@ -31,10 +34,16 @@ export class LanguageSelectorComponent implements OnInit {
         this.currentLang = lang;
       }
     });
+    this.setTranslations();
+    this.translate.onLangChange.subscribe(() => this.setTranslations());
   }
 
   setLanguage(lang: string) {
     this.languageService.setLanguage(lang);
     this.currentLang = lang;
+  }
+
+  private setTranslations() {
+    this.languageLabel = this.translate.instant('COMPONENTS.SHARED.LANGUAGE');
   }
 }
