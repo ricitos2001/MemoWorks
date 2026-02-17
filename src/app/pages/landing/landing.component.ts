7ba2aa2d7b7cdb5cd2e81cdf184569ab8084a939
ricitos2001 <cesar2001ricitos@gmail.com>
2026-02-17 17:26:42 +0100

    pequeña correcion de errores

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,15 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import { Router } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
 import {ThemeService} from '../../services/shared/theme.service';
 import {AuthModalComponent} from '../../components/shared/auth-modal/auth-modal.component';
 import {ButtonComponent} from '../../components/shared/button/button.component';
 import {AuthModalService} from '../../services/shared/auth-modal.service';
 import {distinctUntilChanged} from 'rxjs';
 import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-landing',
@@ -18,12 +22,10 @@ import {TranslateModule, TranslateService} from '@ngx-translate/core';
   styleUrl: '../../../styles/styles.css',
   standalone: true,
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('authModal') authModal!: AuthModalComponent;
+
   darkMode = false;
-
-  constructor(private router: Router, private themeService: ThemeService, private authModalService: AuthModalService, private translate: TranslateService) {}
-
   title = '';
   getStarted = '';
   whatOffers = '';
@@ -33,6 +35,10 @@ export class LandingComponent implements OnInit {
   text2 = '';
   subtitle3 = '';
   text3 = '';
+  // Animations
+  @ViewChild('animatedElement') animatedElement!: ElementRef<HTMLElement>;
+
+  constructor(private router: Router, private themeService: ThemeService, private authModalService: AuthModalService, private translate: TranslateService) {}
 
   ngOnInit(): void {
     this.themeService.currentTheme$
@@ -59,6 +65,25 @@ export class LandingComponent implements OnInit {
 
   onAuthSuccess() {
     this.router.navigate(['/dashboard']);
+  }
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    const group = this.animatedElement.nativeElement;
+    const articles = group.querySelectorAll('.landing__article');
+    const sections = group.querySelectorAll('.landing__section');
+    gsap.set(sections, {opacity: 0, y: 60, scale: 0.98});
+    sections.forEach(section => {
+      gsap.to(section, {opacity: 1, y: 0, scale: 1, duration: 1, ease: 'expo.out', scrollTrigger: {trigger: section, start: 'top 80%', toggleActions: 'play none none reverse'}});
+    })
+    gsap.set(articles, {opacity: 0, y: 60, scale: 0.98});
+    articles.forEach((article) => {
+      gsap.to(article, {opacity: 1, y: 0, scale: 1, duration: 1, ease: 'expo.out', scrollTrigger: {trigger: article, start: 'top 80%', toggleActions: 'play none none reverse'}});
+    });
+  }
+
+  ngOnDestroy(): void {
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
   }
 
   private refreshPicturesBySelector() {
